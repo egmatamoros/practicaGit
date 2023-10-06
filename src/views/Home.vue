19a00032c08aa9c130f65d04db50a7acbc259a23
Akbal-21 <akbal153@hotmail.com>
2023-10-06 10:24:41 -0600

    se cambio el nombnre del titulo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,37 @@
 <template>
   <div class="home">
     <h1>
-      {{title}}
+      {{ title }}
     </h1>
-    <input :type="tipo" v-model="texto" :placeholder="holder"/>
+    <input :type="tipo" v-model="texto" :placeholder="holder" />
     <button @click="ocultarTexto()">Ocultar</button>
-    <button @click="cambiarTitulo(texto)">Cambia el titulo</button>
+    <button @click="cambiarTitulo(texto)">Hola mundo</button>
     <div class="tarjetero">
       <!--Aqui empezo la tarjeta nueva-->
-      <v-card
-        class="mx-auto tarjeta my-2"
-        color="#26c6da"
-        theme="dark"
-        max-width="400"
-        prepend-icon="mdi-twitter"
-        title="Twitter"
-        v-for="(item, key) in info" :key="key"
-      >
+      <v-card class="mx-auto tarjeta my-2" color="#26c6da" theme="dark" max-width="400" prepend-icon="mdi-twitter"
+        title="Twitter" v-for="(item, key) in info" :key="key">
         <v-icon size="x-large">mdi-Pokeball</v-icon>
 
         <v-card-text class="text-h5 py-2">
-          {{item.english}}
+          {{ item.english }}
         </v-card-text>
         <v-card-text class="text-h5 py-2">
-          {{item.spanish}}
+          {{ item.spanish }}
         </v-card-text>
       </v-card>
     </div>
   </div>
 </template>
 <style>
-.tarjetero{
+.tarjetero {
   display: flex;
   width: 100%;
   justify-content: space-between;
   background-color: cyan;
   flex-wrap: wrap;
 }
-.tarjeta{
+
+.tarjeta {
   width: 30%;
   background-color: white;
   border: 1px solid #000;
@@ -51,31 +45,31 @@ export default {
 
   },
   data: () => ({
-    title: "Junue es adorable",
+    title: "Junue es flojo",
     texto: "",
     holder: "Escribe un texto",
     tipo: "password",
     info: []
   }),
   methods: {
-    cambiarTitulo(titulo){
-      this.title =  titulo
+    cambiarTitulo(titulo) {
+      this.title = titulo
     },
-    ocultarTexto(){
-      if(this.tipo === "password") {
+    ocultarTexto() {
+      if (this.tipo === "password") {
         this.tipo = "text"
       } else {
         this.tipo = "password"
       }
     }
   },
-  created(){
+  created() {
     fetch("http://smgpuntosdeventa.net/palabras/")
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      this.info = data.palabras
-    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.info = data.palabras
+      })
   }
 
 }
